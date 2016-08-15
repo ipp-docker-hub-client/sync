c9e96aa5c211d34e2cb5b49fd4ea235f34d80b54
ipp-docker-hub-client <mohsen@iproperty.com>
2016-08-15 16:52:52 +0800

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-a="$1 $2 $3 $4 $5 $6 $7 $8 $9"
+t=$1
+a="$2 $3 $4 $5 $6 $7 $8 $9"
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage: wrapper.sh 10 some-comand"
+    exit 0
+fi
+
 exec $a & pid=$!
-sleep 10
+sleep $1
 kill -9 $pid
 echo "Process code $! terminated with exit code $?"

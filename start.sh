30b09408bfef3565f81188d15f8ea48f41d64d00
ipp-docker-hub-client <mohsen@iproperty.com>
2016-08-16 18:08:24 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-chmod 2775 /data
-chown root:btsync /data
+chmod -R 2775 /data
+chown -R root:btsync /data
 
-chown root:www-data /data/www-data/
+chown -R root:www-data /data/www-data/
 usermod -a -G www-data btsync
 
-chown root:mysql /data/mysql/
+chown -R root:mysql /data/mysql/
 usermod -a -G mysql btsync
 
 start="/usr/bin/btsync --config /var/btsync/sync.conf"

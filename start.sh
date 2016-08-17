435879c04fd8ba015f6e067dca50e5dd2bd198eb
ipp-docker-hub-client <mohsen@iproperty.com>
2016-08-17 22:47:31 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,10 +6,11 @@ chown -R btsync:btsync /var/btsync
 if [ -d "/data" ]; then
   chmod -R 775 /data
   chown -R 0:0 /data
+  chmod g+s /data
   chown -R 0:33 /data/www-data/
-  setfacl -Rm d:g:33:rwx,g:33:rwx /data/www-data/
+  setfacl -d -m g::rwx /data/www-data/
   chown -R 0:105 /data/mysql/
-  setfacl -Rm d:g:105:rwx,g:105:rwx /data/mysql/
+  setfacl -d -m g::rwx /data/mysql/
 fi
 
 #start="/usr/bin/btsync --config /var/btsync/sync.conf --nodaemon"
